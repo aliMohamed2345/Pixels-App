8f4d87780c3326ed1df2490e38253403e3bccf19
ali <eloymohamedalimansor@gmail.com>
2025-01-13 16:17:53 +0200

    some changes

--- src/app/Components/Home/VideosSection.tsx
+++ src/app/Components/Home/VideosSection.tsx
@@ -1,14 +1,12 @@
-import { sampleVideoData } from '@/app/utils/Object';
+import { sampleVideoSrc } from '@/app/utils/Object';
 import Link from 'next/link';
-import Video from '../Video/Video';
-const PhotosSection = () => {
-    const { width, height, src, videoId, duration, videos, type, tags, Favorite, alt, thumbnailSrc } = sampleVideoData
+const VideoSection = () => {
     return (
         <div className=' mx-auto py-20 my-4 bg-secondary'>
             <h5 className='text-2xl sm:text-3xl md:text-4xl  text-text_color text-center my-5 font-bold '>Video</h5>
             <div className=" rounded-lg p-5 bg-primary container mx-auto my-5">
                 <div className="flex  md:flex-row lg:flex-row items-center justify-between gap-2 lg:gap-8 flex-col">
-                    <Video duration={duration} width={width} height={height} src={src} alt={alt} thumbnailSrc={thumbnailSrc} videoId={+videoId} type={type} tags={tags} Favorite={Favorite} videos={videos} />
+                    <video width={640} height={480} src={sampleVideoSrc} controls loop muted className={`rounded-md`} />
                     <p className='lg:w-[60vw] capitalize w-full text-center md:text-left text-text_color text-sm sm:text-lg  '>Engage with High-Quality Videos Access a diverse range of videos, from cinematic clips to dynamic motion graphics. Ideal for filmmakers, marketers, and creators looking to elevate their projects with motion.</p>
                 </div>
                 <Link href={`/videos`} className='text-lg text-white bg-teal-500 shadow-lg hover:bg-teal-600 transition-all hover:scale-105 p-2 block text-center rounded-xl mt-4 mx-auto'>Discover More</Link>
@@ -17,4 +15,4 @@ const PhotosSection = () => {
     );
 }
 
-export default PhotosSection;
+export default VideoSection;
--- src/app/utils/Object.tsx
+++ src/app/utils/Object.tsx
@@ -11,58 +11,8 @@ export const sideMenuOptions: sideMenuProps[] = [
     { name: `Videos`, logo: <IoVideocam /> },
     { name: `Favorites`, logo: <IoHeart /> }
 ]
-export const samplePhotoData = {
-    width: 640,
-    type: 'photo',
-    height: 425,
-    src: `https://pixabay.com/get/g554b890ce65f55803d4ceb3cf38e861b94956e1da825b19934d7ee98ce27c9f64b8e10aea367f82adfca83576cdf7e67df47cdea13065d77efd93ca1437e05cd_640.jpg`,
-    alt: `butterfly, bloom, insects`,
-    tags: `butterfly`,
-    imageId: `9306937`,
-    Favorite: true
-}
-export const sampleVideoData = {
-    tags: `leaves, water, river`,
-    width: 1280,
-    height: 720,
-    src: `https://cdn.pixabay.com/video/2024/11/07/240320_tiny.mp4`,
-    thumbnailSrc: `https://cdn.pixabay.com/video/2024/11/07/240320_tiny.jpg`,
-    duration: 30,
-    type: `film`,
-    videoId: 240320,
-    Favorite: true,
-    alt: `leaves`,
-    videos: {
-        large: {
-            url: 'https://cdn.pixabay.com/video/2024/11/07/240320_large.mp4',
-            width: 3840,
-            height: 2160,
-            size: 160187047,
-            thumbnail: 'https://cdn.pixabay.com/video/2024/11/07/240320_large.jpg'
-        },
-        medium: {
-            url: 'https://cdn.pixabay.com/video/2024/11/07/240320_medium.mp4',
-            width: 2560,
-            height: 1440,
-            size: 91032136,
-            thumbnail: 'https://cdn.pixabay.com/video/2024/11/07/240320_medium.jpg'
-        },
-        small: {
-            url: 'https://cdn.pixabay.com/video/2024/11/07/240320_small.mp4',
-            width: 1920,
-            height: 1080,
-            size: 60491445,
-            thumbnail: 'https://cdn.pixabay.com/video/2024/11/07/240320_small.jpg'
-        },
-        tiny: {
-            url: 'https://cdn.pixabay.com/video/2024/11/07/240320_tiny.mp4',
-            width: 1280,
-            height: 720,
-            size: 28106960,
-            thumbnail: 'https://cdn.pixabay.com/video/2024/11/07/240320_tiny.jpg'
-        }
-    }
-}
+export const sampleVideoSrc = `https://cdn.pixabay.com/video/2024/11/07/240320_tiny.mp4`
+
 export const servicesSectionData = [
     {
         logo: <IoMdDownload size={60} className="p-3 text-green-500 rounded-full bg-primary mx-auto hover:scale-105 transition-all" />,
